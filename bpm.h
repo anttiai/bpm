d13655da1076150bea2b9bae1f3126985ad17edf
Antti Ala-Ilkka <antti@instereo.fi>
2025-02-05 15:23:58 +0200

    Update functions

--- bpm.h
+++ bpm.h
@@ -1,16 +1,17 @@
-#include <stdbool.h>
+#include <stdint.h>
 
-#ifndef FOO_H
-#define FOO_H
+#ifndef BPM_H
+#define BPM_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void bpm_init(int value);
-int bpm_frame_encoded(int track, bool is_keyframe);
-void bpm_frame_lagged(int track);
-void bpm_frame_dropped(int track);
+void bpm_frame_encoded_c(const char* track_fp);
+void bpm_frame_lagged_c(const char* track_fp);
+void bpm_frame_dropped_c(const char* track_fp);
+uint8_t bpm_ts_ptr(uint8_t** ts_data, uint32_t* ts_size);
+void bpm_destroy(uint8_t* data);
 void bpm_print_state();
 
 #ifdef __cplusplus
