b0642aa970ae61c5b52ac3b50b3be071a858f0e5
Antti Ala-Ilkka <antti@instereo.fi>
2025-02-06 14:19:24 +0200

    Simplify example

--- example.c
+++ example.c
@@ -6,53 +6,39 @@ static void print_frame(int track, int frame_number) {
     printf("Track %d, Frame %d\n", track, frame_number);
 }
 
-/*
-    uint8_t buffer[16];  // Allocate buffer in C
-
-    // Call the Rust function to fill the buffer
-    fill_buffer(buffer, 16);
-     */
+void render_and_print_data(int track_idx) {
+    // Get data to a pointer holding the Rust-allocated memory
+    uint8_t* data = NULL;
+    uint32_t size = 0;
+    int result = bpm_render_data_ptr(track_idx, &data, &size);
+    for (int i = 0; i < size; i++) {
+        printf("0x%02X ", data[i]);
+    }
+    bpm_destroy(data);
+}
 
 int main() {
 
-    // Init with two tracks
-    //bpm_init(2);
+    // Two tracks
+    int track0 = bpm_get_track_index("1080p60");
+    int track1 = bpm_get_track_index("720p30");
 
-do {
-    for (int i = 0; i < 130; i++) {
-
-        // Keyframe interval 120 frames
-        bool is_keyframe = (i % 120 == 0);
-
-        int track0 = bpm_get_track_index("1920x1080");
-        int track1 = bpm_get_track_index("1280x720");
-
-        // Track 0: Every frame encoded ("60fps")
+    // 1000 frames
+    for (int i = 0; i < 1000; i++) {
+        // Track 0: Every frame encoded (60fps)
         bpm_frame_encoded(track0);
-        //print_frame(0, i);
 
-        // Track 1: Every other frame encoded ("30fps")
+        // Track 1: Every other frame encoded (30fps)
         if (i % 2 == 0) {
             bpm_frame_encoded(track1);
-            //print_frame(1, i);
+        }
+
+        // Print state and data every 120 frames ("keyframe interval 2s")
+        if (i % 120 == 0) {
+            bpm_print_state();
+            render_and_print_data(track0);
+            render_and_print_data(track1);
         }
     }
-
-    // Print state
-    bpm_print_state();
-
-
-    // Get data
-    uint8_t* ts_data = NULL; // A pointer to hold the Rust-allocated memory
-    uint32_t ts_size = 0;    // A variable to hold the size of the data
-    int result = bpm_ts_ptr(&ts_data, &ts_size); // Pass a pointer to `ts_data`
-
-    // Print the data
-    /*for (int i = 0; i < ts_size; i++) {
-        printf("0x%02X ", ts_data[i]);
-    }*/
-    bpm_destroy(ts_data);
-
-} while (1);
     return 0;
 }
