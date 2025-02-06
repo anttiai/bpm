736271f50b635bc302c80a49c2142a2a8e686190
Antti Ala-Ilkka <antti@instereo.fi>
2025-02-06 14:19:38 +0200

    Clarify function name

--- bpm.h
+++ bpm.h
@@ -11,7 +11,7 @@ int bpm_get_track_index(const char* track_fingerprint);
 void bpm_frame_encoded(int track_idx);
 void bpm_frame_lagged(int track_idx);
 void bpm_frame_dropped(int track_idx);
-uint8_t bpm_data_ptr(int track_idx, uint8_t** ts_data, uint32_t* ts_size);
+uint8_t bpm_render_data_ptr(int track_idx, uint8_t** ts_data, uint32_t* ts_size);
 void bpm_destroy(uint8_t* data);
 void bpm_print_state();
 
