8c83d063ef65683d3ec010558dc929a1202949de
Antti Ala-Ilkka <antti@instereo.fi>
2025-02-07 14:12:52 +0200

    Clarify naming, remove obsolete code

--- example.c
+++ example.c
@@ -2,10 +2,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-static void print_frame(int track, int frame_number) {
-    printf("Track %d, Frame %d\n", track, frame_number);
-}
-
 void render_and_print_data(int track_idx) {
     // Pointers to the Rust-allocated memory
     uint8_t* ts_data = NULL;
@@ -15,6 +11,7 @@ void render_and_print_data(int track_idx) {
     for (int i=0; i<ts_size; i++) {
         printf("0x%02X ", ts_data[i]);
     }
+    printf("\n");
     bpm_destroy(ts_data);
 
     uint8_t* sm_data = NULL;
@@ -24,6 +21,7 @@ void render_and_print_data(int track_idx) {
     for (int i=0; i<sm_size; i++) {
         printf("0x%02X ", sm_data[i]);
     }
+    printf("\n");
     bpm_destroy(sm_data);
 
     uint8_t* erm_data = NULL;
@@ -33,34 +31,34 @@ void render_and_print_data(int track_idx) {
     for (int i=0; i<erm_size; i++) {
         printf("0x%02X ", erm_data[i]);
     }
+    printf("\n");
     bpm_destroy(erm_data);
 }
-
 
 int main() {
     // Two tracks
     int track0 = bpm_get_track_index("1080p60");
     int track1 = bpm_get_track_index("720p30");
 
-    // 1000 frames
-    //for (int i=0; i<1000; i++) {
-    int i = 0;
+    int frame = 0;
     do {
-        i++;
+        frame++;
+
         // Track 0: Every frame encoded (60fps)
         bpm_frame_encoded(track0);
 
         // Track 1: Every other frame encoded (30fps)
-        if (i % 2 == 0) {
+        if (frame % 2 == 0) {
             bpm_frame_encoded(track1);
         }
 
         // Print state and data every 120 frames ("keyframe interval 2s")
-        if (i % 120 == 0) {
+        if (frame % 120 == 0) {
+            printf("\n* Frame %d\n", frame);
             bpm_print_state();
             render_and_print_data(track0);
             render_and_print_data(track1);
         }
-    } while (true);
+    } while (frame < 1000);
     return 0;
 }
